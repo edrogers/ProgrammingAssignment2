dd91b4876c8d6db6e6697682d2ac8e9bc89c88a4
Ed Rogers <f5d2h4eeyw@snkmail.com>
2015-04-23 16:12:27 -0500

    Fixed a small typo. Returning name of one function to original makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,11 @@
 ## function checks for previously cached copies of the inverse before
 ## calling the standard "solve" function.
 
-## "makeMatrix" takes a square matrix as argument. It will return a list 
-## that includes the functions to access and change both the original 
+## "makeCacheMatrix" takes a square matrix as argument. It will return a
+## list that includes the functions to access and change both the original 
 ## matrix and its inverse once it is calculated.
 
-makeMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
   xInverse <- NULL 
 
   # "set" and "get" are accessors to the matrix "x":
@@ -33,7 +33,7 @@ makeMatrix <- function(x = matrix()) {
 }
 
 
-## "cacheSolve" takes the result of "makeMatrix", a list of functions, 
+## "cacheSolve" takes the result of "makeCacheMatrix", a list of functions, 
 ## and returns the inverse of the matrix to which those functions have
 ## access. If the matrix inverse has been computed already, it will use
 ## that value. Otherwise, it will compute it using "solve", and use the
@@ -66,7 +66,7 @@ cacheSolve <- function(x, ...) {
 ## source('~/R/ProgrammingAssignment2/cachematrix.R') 
 ## randomMatrix <- matrix(rnorm(1000000),nrow=1000,ncol=1000)
 ## inverseRandomMatrix <- solve(randomMatrix)
-## cachedRandomMatrix <- makeMatrix(randomMatrix)
+## cachedRandomMatrix <- makeCacheMatrix(randomMatrix)
 ## identical(inverseRandomMatrix,cacheSolve(cachedRandomMatrix))
 ## identical(inverseRandomMatrix,cacheSolve(cachedRandomMatrix))
 ## ## Both "identical" commands should return TRUE, 
